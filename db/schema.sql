2d59c94fe6c81437beb8c3f39526343153287116
drewrvaz <drewrvaz@gmail.com>
2022-10-04 15:03:28 -0400

    Wrote the seed.sql, edited the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,20 @@
+/* Removes any previous databases called employee_db */
 DROP DATABASE IF EXISTS employees_db;
+/* Creates employees_db */
 CREATE DATABASE employees_db;
 
 USE employees_db;
 
+/* Creates department table */
 CREATE TABLE department (
-  id INT NOT NULL PRIMARY KEY,
-  movie_name VARCHAR(30) NOT NULL
+  id INT NOT AUTO_INCREMENT NULL PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
+/* Had to call this position because role is a trigger word in sql apparently? */
+/* Creates position table */
 CREATE TABLE position (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
@@ -18,13 +23,17 @@ CREATE TABLE position (
     ON DELETE SET NULL
 );
 
+/* Creates eployee table */
 CREATE TABLE employee (
-    id INT NOT NULL ,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL ,
     first_name VARCHAR(30) NOT NULL,
     last_name DECIMAL NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES position(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (department_name)
+VALUES ("Engineering"),
+       ("Sales"),
+       ("Legal"),
+       ("Financing");
+
+INSERT INTO position (title, salary, department_id)
+VALUES ("Salesperson", ),
+       ("Sales Manager", ),
+       ("Software Engineer", ),
+       ("Lead Engineer", ),
+       ("Account Manager", ),
+       ("Accountant", ),
+       ("Legal Team Lead", ),
+       ("Lawyer", );
